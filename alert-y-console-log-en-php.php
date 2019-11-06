f170cfe0df7fa0f87cf9384e8099dee08d653c55
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2019-11-06 15:24:54 -0300

    Create alert-y-console-log-en-php.php

---
+++ alert-y-console-log-en-php.php
@@ -0,0 +1,11 @@
+<?php
+
+funcion sc_console_log($texto){
+  echo "<script>console.log('" . $texto . "' );</script>";
+}
+
+function sc_alert($texto){
+ echo "<script>alert('" . $texto . "' );</script>";
+}
+
+?>
