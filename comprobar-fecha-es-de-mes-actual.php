48c8c838e4f2f9e23e179a9eebb34cbedcc7d60d
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2020-01-17 13:34:44 -0300

    Update comprobar-fecha-es-de-mes-actual.php

--- comprobar-fecha-es-de-mes-actual.php
+++ comprobar-fecha-es-de-mes-actual.php
@@ -2,6 +2,6 @@
 function comprobarFechaCaducidad($f1){
   $fecha_actual  = date("01-m-Y 00:00:00",time());
   $fecha_entrada = date("01-m-Y 00:00:00",strtotime($f1));
-  return (strtotime($fecha_actual)>=strtotime($fecha_entrada));
+  return (strtotime($fecha_actual)>strtotime($fecha_entrada));
 }
 ?>
