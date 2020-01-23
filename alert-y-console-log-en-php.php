f55b2a60b2d697079525a08e64e355748c8e94cc
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2020-01-23 09:01:03 -0300

    Update alert-y-console-log-en-php.php

--- alert-y-console-log-en-php.php
+++ alert-y-console-log-en-php.php
@@ -1,11 +1,15 @@
 <?php
 
-funcion sc_console_log($texto){
-  echo "<script>console.log('" . $texto . "' );</script>";
+function convertirStringParaJS($texto){
+    $texto = str_replace('
+','\n',$texto);
+    return str_replace('<br>','\n',$texto);
+}
+function sc_console_log($texto){
+echo "<script>console.log('" .convertirStringParaJS($texto)  . "' );</script>";
 }
 
 function sc_alert($texto){
- echo "<script>alert('" . $texto . "' );</script>";
+echo "<script>alert('" . convertirStringParaJS($texto) . "' );</script>";
 }
-
 ?>
