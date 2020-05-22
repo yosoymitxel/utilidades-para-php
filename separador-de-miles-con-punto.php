61de339120391f482f1cd99a0c46b6a4ebad6b08
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2020-05-22 19:33:58 -0400

    Update separador-de-miles-con-punto.php

--- separador-de-miles-con-punto.php
+++ separador-de-miles-con-punto.php
@@ -2,4 +2,19 @@
     $numero = 21709632;
     $numero = number_format($numero, 0, ",", ".");
     echo($numero);
+//Bonus, formateador de números en formato 1.000,57 (Unidades de mil y decimales)
+function str_to_float($t){
+      $float = floatval(str_replace(',','.',$t));
+      $int   = (int)$float;
+      $float = (strpos(strval($float),'.') !== false)? ','.(int)preg_replace('/d+./','',$float) : '';
+      return number_format($int, 0, ",", ".").$float;
+}
+$var = str_to_float('23619,7833');
+
+var_dump($var);
+
+
+$var = str_to_float('23619');
+
+var_dump($var);
 ?>
