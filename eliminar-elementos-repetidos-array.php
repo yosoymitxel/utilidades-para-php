3b0b88c4219b3e82f368e472e39cf25b94e3e07c
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2020-12-04 18:39:05 -0300

    Create eliminar-elementos-repetidos-array.php

---
+++ eliminar-elementos-repetidos-array.php
@@ -0,0 +1,16 @@
+function sc_arr_eliminar_repetidos($array1,$array2){
+    $arrayValidar      = array_merge($array1,$array2);
+    $arrayElementos    = array_unique(array_merge($array1,$array2));
+    $arrayResult_assoc = array_diff_assoc($arrayValidar, $arrayElementos);
+    $arrayResult = [];
+    
+    array_push($arrayResult,array_keys ($arrayResult_assoc));
+
+    return $arrayResult[0];
+}
+
+$array1 = [1,2,3,4,5];
+$array2 = [1,2,3,6,7];
+
+
+print_r(sc_arr_eliminar_repetidos($array1,$array2));
